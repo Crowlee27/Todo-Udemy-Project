421dea052020d6c171e4340e976ccd753601aeba
Cory Rowlee <rowlee.development@gmail.com>
2023-06-11 01:24:06 -0500

    add component

---
+++ src/Components/CreateTaskForm/_taskTitleField.tsx
@@ -0,0 +1,27 @@
+import { FC, ReactElement } from "react";
+import { TextField } from "@mui/material";
+import { ITextfield } from "./Interfaces/ITextfield";
+import { PropTypes } from "prop-types";
+
+export const TaskTitleField: FC<ITextfield> = (props): ReactElement => {
+  //destructure props
+  const { onChange = (e) => console.log(e), disabled = false } = props; // by adding these it because mandatory to pass these props to the component since they have the ITextfield interface
+  return (
+    <TextField
+      id="title"
+      label="Task Title"
+      placeholder="Task Title"
+      variant="outlined"
+      size="small"
+      name="title"
+      fullWidth
+      disabled={disabled}
+      onChange={onChange}
+    />
+  );
+};
+
+TaskTitleField.propTypes = {
+  onChange: PropTypes.func,
+  disabled: PropTypes.bool,
+};
